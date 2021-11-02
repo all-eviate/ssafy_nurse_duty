1f3a521acbc7899a8d957eacb956b0ef178c11c1
all-eviate <riverboy94@gmail.com>
2021-11-03 01:02:12 +0900

    maybe algorithm, yet to be tested

--- duties/views.py
+++ duties/views.py
@@ -3,9 +3,11 @@ from django.views.decorators.http import require_http_methods, require_POST, req
 from django.contrib.auth.decorators import login_required
 from .models import Nurse, Team
 from .forms import NurseForm
+from schedule_maker import make_monthly_schedule
 import statistics
 import datetime
 import holidays
+from pprint import pprint
 
 """
 알고리즘
@@ -37,119 +39,155 @@ def calendar_get(year, month):
     # 0: 월요일 ~ 6: 일요일 (python 기본 방식이라고 함)
 
 
+example_nurse_info = {
+    1: [1, 0, 1, 0, 0, 0, 0, 0, 2, 0],
+    2: [2, 1, 1, 0, 0, 0, 0, 0, 2, 0],
+    3: [3, 2, 1, 0, 0, 0, 0, 0, 2, 0],
+    4: [4, 0, 1, 0, 0, 0, 0, 0, 2, 0],
+    5: [5, 1, 1, 0, 0, 0, 0, 0, 2, 0],
+    6: [6, 2, 1, 0, 0, 0, 0, 0, 2, 0],
+    # 7: [7, 0, 0, 0, 0, 0, 2, 0],
+    # 8: [8, 0, 0, 0, 0, 0, 2, 0],
+    # 9: [9, 0, 0, 0, 0, 0, 0, 0],
+    # 10:[10, 0, 0, 0, 0, 0, 2, 0],
+    # 11: [11, 0, 0, 0, 0, 0, 2, 0],
+    # 12: [12, 0, 0, 0, 0, 0, 2, 0],
+    # 13: [13, 0, 0, 0, 0, 0, 2, 0]
+}
 
-duties = set() # 근무표의 리스트를 중복제거를 위해 집합으로 선언
-last_duty = ['O', 'D', 'E', 'N'] # 지난달 마지막 주 근무표를 받아와야 함
-duty = ['P'] # 인덱스를 맞추기 위해 [P]adding을 붙여둠
-sample = [10, 7, 7, 7] # 표준편차 계산을 위한 샘플 데이터셋 (수정 예정)
-std = statistics.stdev(sample) # 표준편차 값
-recovery = 0 # 이번 달 recovery off의 지급 여부
-offs = [1, 13, 15, 26] # off 요청의 예시
-holiday = [] # 주말 이외의 공휴일 데이터셋 // 이부분은 어떻게 API나 그런걸 쓰면서 받아와야 할 듯
+def test():
+    nurses_list = Nurse.objects.all()
+    result, modified_nurse_info = make_monthly_schedule(
+    team_list=[1],
+    nurses_list=nurses_list,
+    nurse_info=example_nurse_info,
+    needed_nurses_shift_by_team=1,
+    vacation_info=[],
+    current_month=10,
+    current_date=1,    
+    )
+    print('디버깅용 딕셔너리')
+    pprint(modified_nurse_info)
+    print()
+    i = 1
 
-def dfs(date, month, age, yoil): 
-    global duty
-    global recovery
-    global missed_off
-    global yom
-    # 일단 길이가 다 찼다면
-    if date > month:
-        # 듀티 리스트에 해당 듀티를 추가
-        print(summary)
-        good = True
-        for counts in summary.values():
-            if counts < 5:
-                good = False
-                break
-        if good:
-            duties.add(''.join(duty[1:month + 1]))
-        return
+    for nurse in nurses_list:
+        print(nurse.pk, result[nurse.pk])
 
-    if yoil == 5 or yoil == 6 or date in holiday: # 오늘이 휴일이면 누적 오프 증가
-        missed_off += 1
+test()
 
-    # 오프 받고 싶은 날
-    if date in off_requests:
-        NEXT = ['O']
-    else:
-        if duty[date-1] == 'D':
-            if age > 40:
-                NEXT = ['D', 'E', 'O']
-                if missed_off > 0:
-                    NEXT = ['O', 'D', 'E']
-            else:
-                # 듀티 규칙 [월-2항]
-                if summary['N'] < 7 or (summary['N'] == 7 and not recovery):
-                    NEXT = ['D', 'E', 'N', 'O']
-                    if missed_off > 0:
-                        NEXT = ['O', 'D', 'E', 'N']
-                else:
-                    NEXT = ['D', 'E', 'O']
-                    if missed_off > 0:
-                        NEXT = ['O', 'D', 'E']
-        elif duty[date-1] == 'E':
-            if age > 40:
-                NEXT = ['E', 'O']
-                if missed_off > 0:
-                    NEXT = ['O', 'E']
-            else:
-                # 듀티 규칙 [월-2항]
-                if summary['N'] < 7 or (summary['N'] == 7 and not recovery):
-                    NEXT = ['E', 'N', 'O']
-                    if missed_off > 0:
-                        NEXT = ['O', 'E', 'N']
-                else:
-                    NEXT = ['E', 'O']
-                    if missed_off > 0:
-                        NEXT = ['O', 'E']
-        elif duty[date-1] == 'N': # 이미 40세 이하
-            NEXT = ['O']
-        else: # 전날 오프
-            # N-O-D 방지
-            if duty[date-2] == 'N':
-                NEXT = ['E', 'N', 'O']
-                if missed_off > 0:
-                    NEXT = ['O', 'E', 'N']
-            else:
-                if age > 40:
-                    NEXT = ['D', 'E', 'O']
-                    if missed_off > 0:
-                        NEXT = ['O', 'D', 'E']
-                else:
-                    # 듀티 규칙 [월-2항]
-                    if summary['N'] < 7 or (summary['N'] == 7 and not recovery):
-                        NEXT = ['D', 'E', 'N', 'O']
-                        if missed_off > 0:
-                            NEXT = ['O', 'D', 'E', 'N']
-                    else:
-                        NEXT = ['D', 'E', 'O']
-                        if missed_off > 0:
-                            NEXT = ['O', 'D', 'E']
-    for nextduty in NEXT:
-        if nextduty in ['D', 'E', 'N'] and summary[nextduty] == 7:
-            continue
-        if date >= 4 and nextduty == duty[date-1] == duty[date-2] == duty[date-3]:
-            continue
-        duty.append(nextduty) # += nextduty
-        summary[nextduty] += 1
-        if nextduty == 'N' and summary['N'] == 7 and not recovery:
-            duty.append('O') # += 'O'
-            summary['O'] += 1
-            missed_off -= 1 # 오프를 받았다면 누적 오프 하나 해결
-            recovery = 1
-            dfs(date + 2, month, age, (yoil + 2) % 7)
-            duty.pop() # -= 'O'
-            summary['O'] -= 1
-            missed_off += 1
-            recovery = 0
-        else:
-            if nextduty == 'O':
-                missed_off -= 1
-            dfs(date + 1, month, age, (yoil + 1) % 7)
-            if nextduty == 'O':
-                missed_off += 1
-        summary[nextduty] -= 1
-        duty.pop() # -= nextduty
+# duties = set() # 근무표의 리스트를 중복제거를 위해 집합으로 선언
+# last_duty = ['O', 'D', 'E', 'N'] # 지난달 마지막 주 근무표를 받아와야 함
+# duty = ['P'] # 인덱스를 맞추기 위해 [P]adding을 붙여둠
+# sample = [10, 7, 7, 7] # 표준편차 계산을 위한 샘플 데이터셋 (수정 예정)
+# std = statistics.stdev(sample) # 표준편차 값
+# recovery = 0 # 이번 달 recovery off의 지급 여부
+# offs = [1, 13, 15, 26] # off 요청의 예시
+# holiday = [] # 주말 이외의 공휴일 데이터셋 // 이부분은 어떻게 API나 그런걸 쓰면서 받아와야 할 듯
+
+# def dfs(date, month, age, yoil): 
+#     global duty
+#     global recovery
+#     global missed_off
+#     global yom
+#     # 일단 길이가 다 찼다면
+#     if date > month:
+#         # 듀티 리스트에 해당 듀티를 추가
+#         print(summary)
+#         good = True
+#         for counts in summary.values():
+#             if counts < 5:
+#                 good = False
+#                 break
+#         if good:
+#             duties.add(''.join(duty[1:month + 1]))
+#         return
+
+#     if yoil == 5 or yoil == 6 or date in holiday: # 오늘이 휴일이면 누적 오프 증가
+#         missed_off += 1
+
+#     # 오프 받고 싶은 날
+#     if date in off_requests:
+#         NEXT = ['O']
+#     else:
+#         if duty[date-1] == 'D':
+#             if age > 40:
+#                 NEXT = ['D', 'E', 'O']
+#                 if missed_off > 0:
+#                     NEXT = ['O', 'D', 'E']
+#             else:
+#                 # 듀티 규칙 [월-2항]
+#                 if summary['N'] < 7 or (summary['N'] == 7 and not recovery):
+#                     NEXT = ['D', 'E', 'N', 'O']
+#                     if missed_off > 0:
+#                         NEXT = ['O', 'D', 'E', 'N']
+#                 else:
+#                     NEXT = ['D', 'E', 'O']
+#                     if missed_off > 0:
+#                         NEXT = ['O', 'D', 'E']
+#         elif duty[date-1] == 'E':
+#             if age > 40:
+#                 NEXT = ['E', 'O']
+#                 if missed_off > 0:
+#                     NEXT = ['O', 'E']
+#             else:
+#                 # 듀티 규칙 [월-2항]
+#                 if summary['N'] < 7 or (summary['N'] == 7 and not recovery):
+#                     NEXT = ['E', 'N', 'O']
+#                     if missed_off > 0:
+#                         NEXT = ['O', 'E', 'N']
+#                 else:
+#                     NEXT = ['E', 'O']
+#                     if missed_off > 0:
+#                         NEXT = ['O', 'E']
+#         elif duty[date-1] == 'N': # 이미 40세 이하
+#             NEXT = ['O']
+#         else: # 전날 오프
+#             # N-O-D 방지
+#             if duty[date-2] == 'N':
+#                 NEXT = ['E', 'N', 'O']
+#                 if missed_off > 0:
+#                     NEXT = ['O', 'E', 'N']
+#             else:
+#                 if age > 40:
+#                     NEXT = ['D', 'E', 'O']
+#                     if missed_off > 0:
+#                         NEXT = ['O', 'D', 'E']
+#                 else:
+#                     # 듀티 규칙 [월-2항]
+#                     if summary['N'] < 7 or (summary['N'] == 7 and not recovery):
+#                         NEXT = ['D', 'E', 'N', 'O']
+#                         if missed_off > 0:
+#                             NEXT = ['O', 'D', 'E', 'N']
+#                     else:
+#                         NEXT = ['D', 'E', 'O']
+#                         if missed_off > 0:
+#                             NEXT = ['O', 'D', 'E']
+#     for nextduty in NEXT:
+#         if nextduty in ['D', 'E', 'N'] and summary[nextduty] == 7:
+#             continue
+#         if date >= 4 and nextduty == duty[date-1] == duty[date-2] == duty[date-3]:
+#             continue
+#         duty.append(nextduty) # += nextduty
+#         summary[nextduty] += 1
+#         if nextduty == 'N' and summary['N'] == 7 and not recovery:
+#             duty.append('O') # += 'O'
+#             summary['O'] += 1
+#             missed_off -= 1 # 오프를 받았다면 누적 오프 하나 해결
+#             recovery = 1
+#             dfs(date + 2, month, age, (yoil + 2) % 7)
+#             duty.pop() # -= 'O'
+#             summary['O'] -= 1
+#             missed_off += 1
+#             recovery = 0
+#         else:
+#             if nextduty == 'O':
+#                 missed_off -= 1
+#             dfs(date + 1, month, age, (yoil + 1) % 7)
+#             if nextduty == 'O':
+#                 missed_off += 1
+#         summary[nextduty] -= 1
+#         duty.pop() # -= nextduty
 
 
 """
