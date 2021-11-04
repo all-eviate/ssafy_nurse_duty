0daa93e269f0b2e79822bc760910e1cda7f9e9b9
YC Yoon <z20152002@gmail.com>
2021-11-04 21:32:11 +0900

    before make new model

--- duties/views.py
+++ duties/views.py
@@ -5,6 +5,9 @@ import statistics
 import datetime
 import holidays
 import random
+import calendar
+from .calendar import Calendar
+from django.utils.safestring import mark_safe
 
 """
 알고리즘
@@ -188,10 +191,43 @@ def off_request_save(pk):
 """
 페이지 관리
 """
-
+@login_required
 def index(request):
+    today = get_date(request.GET.get('month'))
 
-    return render(request, 'duties/index.html')
+    prev_month_var = prev_month(today)
+    next_month_var = next_month(today)
+
+    cal = Calendar(today.year, today.month)
+    html_cal = cal.formatmonth(withyear=True, user_pk=request.user.pk)
+    result_cal = mark_safe(html_cal)
+
+    context = {'calendar' : result_cal, 'prev_month' : prev_month_var, 'next_month' : next_month_var}
+
+    return render(request, 'duties/index.html', context)
 
 def dutylist(request):
     return render(request, 'duties/dutylist.html')
+
+
+#현재 달력을 보고 있는 시점의 시간을 반환
+def get_date(req_day):
+    if req_day:
+        year, month = (int(x) for x in req_day.split('-'))
+        return datetime.date(year, month, day=1)
+    return datetime.datetime.today()
+
+#현재 달력의 이전 달 URL 반환
+def prev_month(day):
+    first = day.replace(day=1)
+    prev_month = first - datetime.timedelta(days=1)
+    month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
+    return month
+
+#현재 달력의 다음 달 URL 반환
+def next_month(day):
+    days_in_month = calendar.monthrange(day.year, day.month)[1]
+    last = day.replace(day=days_in_month)
+    next_month = last + datetime.timedelta(days=1)
+    month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
+    return month
