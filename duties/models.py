7428c9f62257824f523194b195f436cbe7393b3a
2dend0713 <2dend0713@gmail.com>
2021-11-04 22:12:27 +0900

    minor change of templates

--- duties/models.py
+++ duties/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Team(models.Model):
     date = models.DateField()
-    duty=models.JSONField(blank=True, null=True)
+    duty = models.JSONField(blank=True, null=True)
 
     def __str__(self):
         return self.date
